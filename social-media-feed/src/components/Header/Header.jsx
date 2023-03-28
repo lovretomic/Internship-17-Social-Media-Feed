a81b8b5ddb65403c12b001dc8838b0562ee2724c
lovretomic <tomiclovre05@gmail.com>
2023-03-29 00:18:34 +0200

    Updated design

--- social-media-feed/src/components/Header/Header.jsx
+++ social-media-feed/src/components/Header/Header.jsx
@@ -1,9 +1,15 @@
 import classes from "./index.module.css";
 
 const Header = () => {
+  const handleTitleClick = () => {
+    window.location.href = "/feed";
+  };
   return (
     <header className={classes.header}>
-      <h1 className={classes.headerTitle}>Social Media Feed</h1>
+      <h1 className={classes.headerTitle} onClick={() => handleTitleClick()}>
+        Social Media Feed
+      </h1>
+      <p className={classes.headerSubtitle}>Internship #17</p>
     </header>
   );
 };
--- social-media-feed/src/pages/PostPage/PostPage.jsx
+++ social-media-feed/src/pages/PostPage/PostPage.jsx
@@ -2,13 +2,17 @@ import { useParams } from "react-router-dom";
 
 import Post from "../../components/Post";
 import Comments from "../../components/Comments";
+import classes from "./index.module.css";
 
 const PostPage = () => {
   const { postId } = useParams();
   return (
     <div>
-      <button onClick={() => (window.location.href = "/feed")}>
-        Back to feed
+      <button
+        className={classes.button}
+        onClick={() => (window.location.href = "/feed")}
+      >
+        {"< Back to feed"}
       </button>
       <Post postId={postId} keyIndex={0} isClickable={false} />
       <h2 style={{ textAlign: "center" }}>Comments</h2>

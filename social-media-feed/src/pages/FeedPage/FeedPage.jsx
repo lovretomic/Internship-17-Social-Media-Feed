77ed635781897febd08b9fda4acaed951ede6fc9
lovretomic <tomiclovre05@gmail.com>
2023-03-28 22:46:12 +0200

    Added styles to feed

--- social-media-feed/src/pages/FeedPage/FeedPage.jsx
+++ social-media-feed/src/pages/FeedPage/FeedPage.jsx
@@ -1,17 +1,29 @@
+import classes from "./index.module.css";
+
 import posts from "../../data/posts.json";
+import users from "../../data/users.json";
 
 posts.sort((a, b) => b.timestamp - a.timestamp);
 
 const FeedPage = () => {
   console.log(posts);
-  return posts.map((post) => (
-    <>
-      <img src={post.profilePic} />
-      <h1>{post.userId}</h1>
-      <p>{post.text}</p>
-      <hr />
-    </>
-  ));
+  return posts.map((post, i) => {
+    const userData = users.find((user) => user.id === post.userId);
+    return (
+      <div className={classes.post} key={i}>
+        <div className={classes.postHeader}>
+          <img
+            className={classes.postImg}
+            src={userData.profilePic}
+            alt="profilePic"
+          />
+          <p className={classes.postUsername}>{userData.username}</p>
+        </div>
+
+        <p className={classes.postText}>{post.text}</p>
+      </div>
+    );
+  });
 };
 
 export default FeedPage;

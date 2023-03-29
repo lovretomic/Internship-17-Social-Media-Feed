72586797b253368fd5038bc22ec55a12d0f103bc
lovretomic <tomiclovre05@gmail.com>
2023-03-29 13:26:44 +0200

    Updated 404 page

--- social-media-feed/src/pages/NotFound/NotFound.jsx
+++ social-media-feed/src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,15 @@
+import classes from "./index.module.css";
+
 const NotFound = () => {
-  return <h1>404 - Not found</h1>;
+  return (
+    <div className={classes.container}>
+      <h1 className={classes.title}>404</h1>
+      <p className={classes.subtitle}>Resource not found.</p>
+      <a href="/feed" className={classes.link}>
+        Return to feed
+      </a>
+    </div>
+  );
 };
 
 export default NotFound;

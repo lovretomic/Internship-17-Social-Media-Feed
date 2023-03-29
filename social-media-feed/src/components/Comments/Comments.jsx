e529da04f0bc518b4dbd7c57983e0f86d40ca139
lovretomic <tomiclovre05@gmail.com>
2023-03-29 22:41:02 +0200

    More UX improvements

--- social-media-feed/src/components/Comments/Comments.jsx
+++ social-media-feed/src/components/Comments/Comments.jsx
@@ -22,11 +22,17 @@ const Comments = ({ postId }) => {
         </div>
 
         <p className={classes.postText}>{comment.body}</p>
-        <p
-          className={classes.postTimestamp}
-        >{`Posted on ${postTimestamp.getDate()}/${
-          postTimestamp.getMonth() + 1
-        }/${postTimestamp.getFullYear()} at ${postTimestamp.getHours()}:${postTimestamp.getSeconds()}`}</p>
+        <p className={classes.postTimestamp}>
+          {`Posted on ${postTimestamp.getDate()}/${
+            postTimestamp.getMonth() + 1
+          }/${postTimestamp.getFullYear()} at ${
+            (postTimestamp.getHours() < 10 ? "0" : "") +
+            postTimestamp.getHours()
+          }:${
+            (postTimestamp.getSeconds() < 10 ? "0" : "") +
+            postTimestamp.getSeconds()
+          }`}
+        </p>
       </div>
     );
   });
